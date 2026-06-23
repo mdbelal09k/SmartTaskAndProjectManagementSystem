869f684c2578e126f2941cb40254470124604058
mygitbhubmd <mdbelalquraishi9546@gmail.com>
2026-06-23 10:03:50 +0530

    update all files

---
+++ src/main/java/com/SmartTaskAndProjectManagementSystem/SmartTaskAndProjectManagementSystem/SmartTaskAndProjectManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.SmartTaskAndProjectManagementSystem.SmartTaskAndProjectManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskAndProjectManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTaskAndProjectManagementSystemApplication.class, args);
+		System.out.println("project is the running!");
+	}
+
+}
---
+++ src/test/java/com/SmartTaskAndProjectManagementSystem/SmartTaskAndProjectManagementSystem/SmartTaskAndProjectManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartTaskAndProjectManagementSystem.SmartTaskAndProjectManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaskAndProjectManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
